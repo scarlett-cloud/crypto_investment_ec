f13d4ce5545e82361140a8b8ab2dbd4ffe31b827
scarlett-cloud <94936620+scarlett-cloud@users.noreply.github.com>
2022-03-03 17:31:04 -0500

    Update mgogasociados.php

--- mgogasociados.php
+++ mgogasociados.php
@@ -1,11 +1,11 @@
 <?php
 $mystring1 = "CRYPTO = IOTX" . PHP_EOL;
-$mystring2 = "CRYPTO = ROSE" . PHP_EOL;
-$mystring3 = "CRYPTO = SHINA INU" . PHP_EOL;
-$mystring4 = "CRYPTO = ECASH (XEC)" . PHP_EOL;
-$mystring5 = "CRYPTO = BUSD" . PHP_EOL;
-$mystring6 = "CRYPTO = AXIE INFINITY (AXS)" . PHP_EOL;
-$mystring7 = "CRYPTO = SAITAMA";
+$mystring2 = "//CRYPTO = ROSE" . PHP_EOL;
+$mystring3 = "//CRYPTO = SHINA INU" . PHP_EOL;
+$mystring4 = "//CRYPTO = ECASH (XEC)" . PHP_EOL;
+$mystring5 = "//CRYPTO = BUSD" . PHP_EOL;
+$mystring6 = "//CRYPTO = AXIE INFINITY (AXS)" . PHP_EOL;
+$mystring7 = "//CRYPTO = SAITAMA";
 $mystring1 .= $mystring2 .= $mystring3 .= $mystring4 .= $mystring5 .= $mystring6 .= $mystring7;
 echo($mystring1);
 ?>
